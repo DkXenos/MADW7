acb629a3d17c10d193b2330ca405277785c9e481
DkXenos <superjason101010@gmail.com>
2026-04-10 07:12:32 +0700

    init first

---
+++ LabWeek7/LabWeek7App.swift
@@ -0,0 +1,17 @@
+//
+//  LabWeek7App.swift
+//  LabWeek7
+//
+//  Created by Jason TIo on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LabWeek7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
