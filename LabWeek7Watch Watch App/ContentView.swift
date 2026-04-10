65246ed6e60278540be40a63ecac8e61bfb6241c
DkXenos <superjason101010@gmail.com>
2026-04-10 07:55:06 +0700

    watchOS ready test

---
+++ LabWeek7Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LabWeek7Watch Watch App
+//
+//  Created by Jason TIo on 10/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LabWeek7Watch Watch App/LabWeek7WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabWeek7WatchApp.swift
+//  LabWeek7Watch Watch App
+//
+//  Created by Jason TIo on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LabWeek7Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
